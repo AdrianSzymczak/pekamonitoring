9b90f0bbd79cdce57b7e5831f731f7de5b2283bc
wozniakbartosz <bartosz.jk.wozniak@gmail.com>
2015-06-06 16:51:51 +0200

    first nvd3 graph, some cleaning around templates

--- viz/web/index.py
+++ viz/web/index.py
@@ -24,15 +24,36 @@ def lines():
 	results['values'] = [[i['_id'], i['count']] for i in lines]
 	return render_template('lines.html', results=results)
 
-@app.route("/stop/<name>")
-def stop(name):
-	stops = collection.aggregate([{"$match": {"time.line": name}},{"$group" : { "_id" : "$bollard.name", "count" : { "$sum": 1}}},{"$sort" : {"_id": 1}}])
+@app.route("/stops/<line>")
+def stops(line):
+	stops = collection.aggregate([{"$match": {"time.line": line}},{"$group" : { "_id" : "$bollard.name", "count" : { "$sum": 1}}},{"$sort" : {"_id": 1}}])
 	
 	results = {}
 	results['keys'] = 'name', 'count'
 	results['values'] = [[i['_id'], i['count']] for i in stops]
+	results['line'] = line
 	return render_template('stops.html', results=results)
 
+@app.route("/stops/<line>/<stop>/all")
+def stop(line, stop):
+	stop = collection.find({"time.line": str(line), "bollard.name": str(stop)})
+	
+	results = {}
+	results['keys'] = ['details']
+	results['values'] = [[i] for i in stop]
+	return render_template('stop_all.html', results=results)
+
+@app.route("/stops/<line>/<stop>")
+def stop_graph(line, stop):
+	minutes = collection.aggregate([{"$match": {"time.line": str(line), "bollard.name": str(stop)}}, {"$group": {"_id": "$time.minutes", "count": {"$sum": 1}}},{"$sort" : {"_id": 1}}])
+	# minutes = collection.aggregate([{"$match": {"time.line": str(line)}},{"bollard.name": str(stop)}}, {"$group": {"_id": "times.minutes", "count": {"$sum": 1}}}])
+
+	results = {}
+	results['keys'] = 'minutes', 'count'
+	results['values'] = [[i['_id'], i['count']] for i in minutes]
+	results['line'] = line
+	results['stop'] = stop
+	return render_template('stop_graph.html', results=results)
 if __name__ == "__main__":
   app.run(debug=True)
 
